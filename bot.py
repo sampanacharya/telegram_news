a1d67c96642dec8d69b36a22c884548a8f83b41c
Sampan Acharya <sampanacharya@Sampans-Mac.local>
2023-06-23 16:56:57 +0530

    Error Blocks

--- bot.py
+++ bot.py
@@ -38,28 +38,30 @@ def updateMe(message):
         if(len(news_set) == 10):
             news_set = []
             continue
-
-        news_dict = feed_fetcher(RSS_FEEDS[idx])
-        etag = etag_gen(news_dict['title'])
-        #print(etag, news_set)
-        if(etag not in news_set):
-            
-            rewritten_Dict = article_rewrite(news_dict)
-            
-            news_set.append(etag)
-            data.append(rewritten_Dict['title'])
-
-            reply = rewritten_Dict['title'] + '\n' + '\n' + "----------------------" + "\n"
-            reply += rewritten_Dict['paragraph'] + '\n'
-            reply += 'Source: ' + news_dict['link'] + '\n\n'
-            reply += 'Published On: ' + news_dict['published'] + '\n'
-
-            sent = sentiment_inference(rewritten_Dict['title'])
-            reply += '\n Sentiment Rating:\n ' + str(sent) + '\n' 
-            reply += 'NOTE:\n-Sentiment Rating is powered by distilroberta model and might be under inspection\n-All the ratings provided are out of 1'
-            bot.send_message("@cryptoNarad", reply)
-        idx += 1
         
+        try:
+            news_dict = feed_fetcher(RSS_FEEDS[idx])
+            etag = etag_gen(news_dict['title'])
+            #print(etag, news_set)
+            if(etag not in news_set):
+                
+                rewritten_Dict = article_rewrite(news_dict)
+                
+                news_set.append(etag)
+                data.append(rewritten_Dict['title'])
+
+                reply = rewritten_Dict['title'] + '\n' + '\n' + "----------------------" + "\n"
+                reply += rewritten_Dict['paragraph'] + '\n'
+                reply += 'Source: ' + news_dict['link'] + '\n\n'
+                reply += 'Published On: ' + news_dict['published'] + '\n'
+
+                sent = sentiment_inference(rewritten_Dict['title'])
+                reply += '\n Sentiment Rating:\n ' + str(sent) + '\n' 
+                reply += 'NOTE:\n-Sentiment Rating is powered by distilroberta model and might be under inspection\n-All the ratings provided are out of 1'
+                bot.send_message("@cryptoNarad", reply)
+            idx += 1
+        except:
+            pass       
 
 @bot.message_handler(commands = ['stop'])
 def stop(message):
